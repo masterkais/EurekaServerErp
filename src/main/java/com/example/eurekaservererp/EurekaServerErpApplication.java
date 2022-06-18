049d106db75ac671b7fb57d81f90dc17882aa2c4
ktrabelsi <ktrabelsi@spark.local>
2022-06-18 14:05:54 +0200

    first commit

---
+++ src/main/java/com/example/eurekaservererp/EurekaServerErpApplication.java
@@ -0,0 +1,15 @@
+package com.example.eurekaservererp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eurekaservererp/EurekaServerErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaservererp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
